561f461ec0a9f707dd5210d145b1d69e054a27b9
unknown <davidbaicoloradocollege@gmail.com>
2018-01-29 14:47:03 -0700

    KNN sublcass

--- Ensemble_Learning/KNN.py
+++ Ensemble_Learning/KNN.py
@@ -1,7 +1,13 @@
+import numpy as np
+# import mglearn
+from sklearn.neighbors import KNeighborsClassifier
+
 class KNN(MLModel):
+    def __init__(self, k): #, OTHER PARAMS):
+        self.knn = KNeighborsClassifier(n_neighbors = k)
+
     def train(self, x_train, y_train):
-        self.knn = KNeighborsClassifier(n_neighbors=k)
         self.knn.fit(self.x_train, self.y_train)
 
     def report_accuracy(self, x_text, y_test):
-        self.knn.score(self.x_test, self.y_test)
+        return self.knn.score(self.x_test, self.y_test)
