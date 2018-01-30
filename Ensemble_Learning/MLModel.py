45d169ec0269157109634eb47d371b00ef10ef37
unknown <davidbaicoloradocollege@gmail.com>
2018-01-30 02:15:47 -0700

    WIP statistical analysis of a model

--- Ensemble_Learning/MLModel.py
+++ Ensemble_Learning/MLModel.py
@@ -1,5 +1,5 @@
-from sklearn.model_selection import train_test_split 
-from abc import ABCMeta, abstractmethod 
+from sklearn.model_selection import train_test_split
+from abc import ABCMeta, abstractmethod
 import mglearn
 
 class MLModel:
@@ -15,3 +15,23 @@ class MLModel:
 
     def split_data(self, inputs, outputs, test_size=.25, ):
         return train_test_split( inputs, outputs, test_size=test_size)
+
+    # # TODO: Implemented the math, but nothing else so WIP
+    # def run_trials(self):
+    #     trial_n = 10
+    #     accuracies = []
+    #
+    #     avg = 0.0
+    #     for i in range(trial_n):
+    #         self.split_data()
+    #         self.train()
+    #         acc = self.report_accuracy()
+    #         accuracies.append( acc )
+    #         avg += acc
+    #     avg /= trial_n
+    #
+    #     std_dev = 0.0
+    #     for i in range(trial_n):
+    #         std_dev += Math.pow( accuracies[i]-avg, 2 )
+    #     std_dev /= trial_n
+    #     std_dev = Math.sqrt(std_dev)
