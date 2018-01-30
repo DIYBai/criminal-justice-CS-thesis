0102bc6393c1012af9a52e8bd8df9e6e05fb8fc1
Long Nguyen <long.nguyen@coloradocollege.edu>
2018-01-30 13:52:34 -0700

    fixes

--- Ensemble_Learning/testScript.py
+++ Ensemble_Learning/testScript.py
@@ -1,13 +1,20 @@
 import RiskParser as rp
 from ANN import ANN
+from Dtree import Dtree
 
 
 inputs, outputs = rp.parse_data()
 print(inputs)
 print(outputs)
 
-print("### ANN Test ###")
-test_ANN = ANN()
-x_train, x_test, y_train, y_test = test_ANN.split_data(inputs, outputs, .25)
-test_ANN.train(x_train, y_train)
-print("Accuracy: " + str(test_ANN.report_accuracy(x_test,y_test)))
+# print("### ANN Test ###")
+# test_ANN = ANN()
+# x_train, x_test, y_train, y_test = test_ANN.split_data(inputs, outputs, .25)
+# test_ANN.train(x_train, y_train)
+# print("Accuracy: " + str(test_ANN.report_accuracy(x_test,y_test)))
+
+print("### Dtree Test ###")
+test_Dtree = Dtree()
+x_train, x_test, y_train, y_test = test_Dtree.split_data(inputs, outputs, .25)
+test_Dtree.train(x_train, y_train)
+print("Accuracy: " + str(test_Dtree.report_accuracy(x_test,y_test)))
