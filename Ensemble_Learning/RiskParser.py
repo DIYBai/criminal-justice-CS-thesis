4bc65a145b36914c3345678a0bc2b5e311758260
unknown <davidbaicoloradocollege@gmail.com>
2018-02-06 14:35:24 -0700

    Generalized csv parses

--- Ensemble_Learning/RiskParser.py
+++ Ensemble_Learning/RiskParser.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-def parse_data(filename="RiskAssessData.csv"):
+#default param values are for RiskAssessData.csv
+def parse_data(filename="RiskAssessData.csv", firstInpCol = 4, lastInpCol = 33, outCol = -1):
     riskAsses = pd.read_csv(filename, header = 0)
-    original_headers = list(riskAsses.columns.values)
+    # original_headers = list(riskAsses.columns.values) #unused
     riskAsses = riskAsses._get_numeric_data()
     RA_array = riskAsses.as_matrix()
-    return RA_array[:,4:33], RA_array[:,len(RA_array[0])-1]
+    if outCol == -1:
+        outCol = len(RA_Array[0]) - 1
+    return RA_array[:, firstInpCol:lastInpCol], RA_array[:, outCol]
