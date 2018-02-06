49caade77360e38e6d502296f9473a7992f9c517
unknown <davidbaicoloradocollege@gmail.com>
2018-02-06 14:47:22 -0700

    Fixed small bug

--- Ensemble_Learning/RiskParser.py
+++ Ensemble_Learning/RiskParser.py
@@ -7,5 +7,5 @@ def parse_data(filename="RiskAssessData.csv", firstInpCol = 4, lastInpCol = 33, 
     riskAsses = riskAsses._get_numeric_data()
     RA_array = riskAsses.as_matrix()
     if outCol == -1:
-        outCol = len(RA_Array[0]) - 1
+        outCol = len(RA_array[0]) - 1
     return RA_array[:, firstInpCol:lastInpCol], RA_array[:, outCol]
