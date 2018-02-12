45071163423d0685ce711d0a1040d59d71b0dabb
unknown <davidbaicoloradocollege@gmail.com>
2018-02-11 20:37:49 -0700

    Added k-means clustering diagnostics

--- Ensemble_Learning/db-cluster.py
+++ Ensemble_Learning/db-cluster.py
@@ -3,6 +3,15 @@ import RiskParser as rp
 from sklearn.cluster import DBSCAN
 from sklearn.cluster import KMeans
 
+#for timing purposes
+from time import time
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+
 #needed for breast cancer data
 # from sklearn.preprocessing import StandardScaler
 # in_file = "../Data/breast_cancer.csv"
@@ -13,9 +22,31 @@ in_file = "../Data/RiskAssessData.csv"
 inputs, outputs = rp.parse_data(in_file)
 
 #below gotten from http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#sphx-glr-auto-examples-cluster-plot-dbscan-py
-db = DBSCAN(eps=0.5, min_samples=75).fit(inputs)
+db = DBSCAN(eps=1, min_samples=75).fit(inputs)
 labels = db.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0) # Number of clusters in labels, ignoring noise if present.
 # print(labels)
-print( np.count_nonzero(labels == -1) ) #https://stackoverflow.com/questions/28663856/how-to-count-the-occurrence-of-certain-item-in-an-ndarray-in-python
+print("** DBSCAN **")
+print( "Outliers:", np.count_nonzero(labels == -1) ) #https://stackoverflow.com/questions/28663856/how-to-count-the-occurrence-of-certain-item-in-an-ndarray-in-python
 print('Estimated number of clusters: %d' % n_clusters_)
+
+
+print("\n** K-Means **")
+#TODO: cluter w k-means
+# below code from http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_digits.html#sphx-glr-auto-examples-cluster-plot-kmeans-digits-py
+def bench_k_means(estimator, name, data):
+    t0 = time()
+    estimator.fit(data)
+    print('%-9s\t%.2fs\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'#\t%.3f'
+          % (name, (time() - t0), estimator.inertia_,
+             metrics.homogeneity_score(labels, estimator.labels_),
+             metrics.completeness_score(labels, estimator.labels_),
+             metrics.v_measure_score(labels, estimator.labels_),
+             metrics.adjusted_rand_score(labels, estimator.labels_),
+             metrics.adjusted_mutual_info_score(labels,  estimator.labels_)#,
+             # metrics.silhouette_score(data, estimator.labels_, metric='euclidean', sample_size=sample_size)
+            )
+         )
+
+for i in range(1, 30):
+    bench_k_means(KMeans(init = 'k-means++', n_clusters = i, n_init = 20), name = ("k-means" + str(i)), data = inputs)
