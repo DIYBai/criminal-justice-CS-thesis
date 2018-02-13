97affc872dcde410308f6fa72b33fa2ac799feb7
unknown <davidbaicoloradocollege@gmail.com>
2018-02-13 16:13:19 -0700

    Vector input testing

--- Data/db-vectorWriter.py
+++ Data/db-vectorWriter.py
@@ -2,8 +2,10 @@ import csv
 
 with open('vectorData.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    cols = 40
+    cols = 29
+    writer.writerow( ['0.0']*cols + ['-1.0'] )
     for i in range(cols):
-        writer.writerow( ['-1']*3 + ['0']*i + ['1'] + ['0']*(cols - i - 1) + ['-1'] )
+        writer.writerow( ['0.0']*i + ['1.0'] + ['0.0']*(cols - i - 1) + ['-1.0'] )
+    writer.writerow( ['1.0']*cols + ['-1.0'] )
     for i in range(cols):
-        writer.writerow( ['-1']*3 + ['1']*i + ['0'] + ['1']*(cols - i - 1) + ['-1'] )
+        writer.writerow( ['1.0']*i + ['0.0'] + ['1.0']*(cols - i - 1) + ['-1.0'] )
