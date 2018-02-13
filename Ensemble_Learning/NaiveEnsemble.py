7e38f7b97f1345e10d513d2d70aedabca8d78f8c
unknown <davidbaicoloradocollege@gmail.com>
2018-02-13 16:14:22 -0700

    Improved get probability method

--- Ensemble_Learning/NaiveEnsemble.py
+++ Ensemble_Learning/NaiveEnsemble.py
@@ -11,17 +11,33 @@ class NaiveEnsemble(EnsembleModel.EnsembleModel):
 
     def train(self, x_train, y_train):
         for i in range(len(self.model_list)):
+            # print("Training", self.model_list[i])
             self.model_list[i].train(x_train,y_train)
 
     def predict(self, x_test):
         predictions = []
         for i in range(len(self.model_list)):
             prediction = self.model_list[i].predict(x_test)
+            # print(prediction)
             predictions.append(prediction)
 
         predictions_np = np.asarray(predictions)
         m = stats.mode(predictions_np)
         return m.mode[0]
+
+    #similar functionality to predict BUT returns the prediction matrix, not just the actual prediction
+    #this is so that other code can access the component models' predictions
+    #to get the actual prediction, use (np.average(FUNCTIONOUTPUT, axis = 0))[0][0]
+    def predict_prob(self, x_test):
+        probabilities = []
+        for i in range(len(self.model_list)):
+            prob = self.model_list[i].predict_prob(x_test)
+            # print(self.model_list[i], '\t', '{:.3f}'.format(prob[0][0]) ) #temporary testing
+            probabilities.append(prob)
+
+        probs_np = np.asarray(probabilities)
+        # return np.average(probs_np, axis = 0)
+        return probs_np
 
     def report_accuracy(self, x_test, y_test):
         count = 0
