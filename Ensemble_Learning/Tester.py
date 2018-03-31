7187dc630fdfa8d09aacbaf0e64cd25f744a51d4
Long Nguyen <long.nguyen@coloradocollege.edu>
2018-03-31 15:45:40 -0600

    Tester abstract class

---
+++ Ensemble_Learning/Tester.py
@@ -0,0 +1,14 @@
+from sklearn.model_selection import train_test_split
+from abc import ABCMeta, abstractmethod
+import RiskParser as rp
+import mglearn
+
+class Tester:
+    __metaclass__ = ABCMeta
+
+    def parse_data(self, filename, first, last):
+        return rp.parse_data(filename=filename,firstInpCol = first, lastInpCol = last)
+
+    @abstractmethod
+    def run_trials(self, model,iterations):
+       return
