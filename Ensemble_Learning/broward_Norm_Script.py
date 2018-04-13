c1695188eaee44e6efa9c6438140067bc03f3b9f
Callum <neeson.callum@gmail.com>
2018-04-13 13:34:24 -0600

    cleaned files

---
+++ Ensemble_Learning/broward_Norm_Script.py
@@ -0,0 +1,89 @@
+import RiskParser as rp
+import os
+import sys
+from NaiveEnsemble import NaiveEnsemble
+from discreteSL import discreteSL
+from annSL import annSL
+from fullSL import fullSL
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from scipy import stats
+lib_path = os.path.abspath(os.path.join('..','ComponentModels'))
+sys.path.append(lib_path)
+from ANN import ANN
+from Dtree import Dtree
+from KNN import KNN
+from LogR import LogR
+from slTester import slTester
+
+## ---Component ML Models--- ## 
+a_DTree = Dtree('gini','best',3)
+b_DTree = Dtree('entropy','best',3)
+c_DTree = Dtree('entropy','random',3)
+a_KNN = KNN(11,'uniform','auto')
+b_KNN = KNN(15, 'uniform','kd_tree')
+c_KNN = KNN(23, 'uniform','ball_tree')
+a_LogR = LogR(algo='newton-cg', c=3)
+b_LogR = LogR(algo='newton-cg', c=6)
+c_LogR = LogR(algo='newton-cg', c=9)
+a_ANN = ANN(iteration=8700,layers=[100])
+b_ANN = ANN(iteration=2700,layers=[100,90,80,70])
+inputs, outputs = rp.parse_data("../Data/broward_norm.csv",4,10,13)
+model_list = [a_DTree, b_DTree, c_DTree, a_KNN, b_KNN, c_KNN, a_KNN, b_KNN, c_KNN, a_LogR, b_LogR, c_LogR, a_ANN, b_ANN]
+##inputs, outputs = rp.parse_data("../Data/breast_cancer.csv",1,9,10)
+
+
+## ---Ensemble Tester--- ##
+x_train, x_test, y_train, y_test = a_ANN.split_data(inputs, outputs, .25)
+test_fullSL = annSL([a_DTree, b_DTree, a_KNN, b_KNN, a_LogR, b_LogR, a_ANN, b_ANN])
+sl_tester = slTester()
+
+print("Dataset: Breast Cancer ")
+print("Input: standard")
+print("Predict: malignant or not")
+print("------------------------------")
+inputs, outputs = rp.parse_data("../Data/breast_cancer.csv",1,9,10)
+sl_tester.run_trials("breast_cancer",test_fullSL,5,inputs,outputs)
+
+print("Dataset: Broward Norm ")
+print("Input: no compas desc, no race ")
+print("Predict: Actual Recidivism")
+print("------------------------------")
+inputs, outputs = rp.parse_data("../Data/broward_norm.csv",4,9,13)
+sl_tester.run_trials("broward_norm_no_compas_no_race",test_fullSL,5,inputs,outputs)
+
+print("Dataset: Broward Norm ")
+print("Input: with compas desc, no race ")
+print("Predict: Actual Recidivism")
+print("------------------------------")
+inputs, outputs = rp.parse_data("../Data/broward_norm.csv",4,10,13)
+sl_tester.run_trials("broward_norm_with_compas_no_race",test_fullSL,5,inputs,outputs)
+
+print("Dataset: Broward Norm ")
+print("Input: no compas desc, with race ")
+print("Predict: Actual Recidivism")
+print("------------------------------")
+inputs, outputs = rp.parse_data("../Data/broward_norm.csv",3,9,13)
+sl_tester.run_trials("broward_norm_no_compas__with_race",test_fullSL,5,inputs,outputs)
+
+print("Dataset: Broward Norm ")
+print("Input: with compas desc, with race ")
+print("Predict: Actual Recidivism")
+print("------------------------------")
+inputs, outputs = rp.parse_data("../Data/broward_norm.csv",3,10,13)
+sl_tester.run_trials("broward_norm_with_compas__with_race",test_fullSL,5,inputs,outputs)
+
+print("Dataset: Broward Norm ")
+print("Input: without compas desc")
+print("Predict: Race")
+print("------------------------------")
+inputs, outputs = rp.parse_data("../Data/broward_norm.csv",4,9,3)
+sl_tester.run_trials("broward_norm_no_compas_predict_race",test_fullSL,5,inputs,outputs)
+
+print("Dataset: Broward Norm ")
+print("Input: with compas desc")
+print("Predict: Race")
+print("------------------------------")
+inputs, outputs = rp.parse_data("../Data/broward_norm.csv",4,10,3)
+sl_tester.run_trials("broward_norm_with_compas__predict_race",test_fullSL,5,inputs,outputs)
+
--- Ensemble_Learning/slTester.py
+++ Ensemble_Learning/slTester.py
@@ -18,34 +18,37 @@ import numpy as np
 
 class slTester(Tester.Tester):
 
-##handel convergence warning 
-##too string method in each component model
-
+    ##  get metrics on input model's prediction  ##
     def test_sample(self,model,inputs,outputs):
-        model.train(inputs, outputs)
         predictions = model.predict(inputs)
         tn, fp, fn, tp = confusion_matrix(predictions,outputs).ravel()
         a_score = accuracy_score(predictions,outputs)
         return a_score,[tn,fp,fn,tp]
 
+    ##  returns confusion matrix and accuracy score  ##
     def update_metrics(self, cm, conf, acc, a):
-    	##print(acc)
         update_cm = [x + y for x, y in zip(conf, cm)]
         acc.append(a)
         return update_cm, acc
 
+    ##  prevents error with divisor of 0  ##
+    def safe_division(self,a,b):
+        if(b > 0):
+            return (float(a)/float(b))
+        else: return 0
 
-    def get_result_metrics(self, accuracies, cm, iterations):
-
+    ##  returns fomratted data for csv output, including false positive and false negative rates  ##
+    def get_result_metrics(self,filename, model,test_set, accuracies, cm, iterations):
         final_cm = [x / iterations for x in cm]
-    	std = np.std(accuracies)
-    	fp_rate = final_cm[1]/(final_cm[1]+final_cm[2])
-    	fn_rate = final_cm[2]/(final_cm[1]+final_cm[2])
+        print("accuracies: " + str(accuracies))
+    	std = np.std(accuracies,ddof=1)
+        fp_rate = self.safe_division(final_cm[1],(final_cm[0] + final_cm[1]))
+        fn_rate = self.safe_division(final_cm[2],(final_cm[3] + final_cm[2]))
     	acc = (np.sum(accuracies))/iterations
 
-    	return [str(acc),str(std),str(fp_rate),str(fn_rate)]
+    	return [str(model),filename,test_set, str(acc),str(std),str(fp_rate),str(fn_rate)]
 
-
+    ##  trains and runs model on input/output and exports performance metrics to filename  ##
     def run_trials(self,filename, model, iterations, inputs, outputs):
         parser = ANN()
         train_acc = [0]
@@ -54,23 +57,24 @@ class slTester(Tester.Tester):
         test_cm = [0,0,0,0]
         for i in range(iterations):
             x_train, x_test, y_train, y_test = parser.split_data(inputs, outputs, .25)
+            model.train(x_train, y_train)
             train_a, train_conf = self.test_sample(model, x_train, y_train)
             test_a, test_conf = self.test_sample(model, x_test, y_test)
             train_cm, train_acc = self.update_metrics(train_cm, train_conf, train_acc, train_a)
             test_cm, test_acc = self.update_metrics(test_cm, test_conf, test_acc, test_a)
         
-        test_metrics = self.get_result_metrics(test_acc,test_cm,iterations)
-        train_metrics = self.get_result_metrics(train_acc,train_cm,iterations)
+        test_metrics = self.get_result_metrics(filename, model,"test", test_acc,test_cm,iterations)
+        train_metrics = self.get_result_metrics(filename, model,"train", train_acc,train_cm,iterations)
         self.export_results(filename, test_metrics, train_metrics)
-        
         return 
 
-    ##, test_metrics, train_metrics
-    def export_results(self,filename, test_metrics, train_metrics):
-        ##print(test_metrics)
-        ##print(train_metrics)
-        with open(r'Ensemble_Metrics.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(test_metrics)
+    ##  exports test metrics and train metrics to filename
+    def export_results(self,filename,test_metrics, train_metrics):
+        print(test_metrics)
+        print(train_metrics)
+        res = [test_metrics, train_metrics]
+        my_df = pd.DataFrame(res)
+        output_file = 'slTester_Metrics/' + filename + '.csv'
+        my_df.to_csv(output_file, header=False)
         return
 
